2167a4b9f56232d30e9bbd064304a09f69c7bac9
Johnny Squardo <johnnysquardo@gmail.com>
2019-12-09 13:27:58 -0500

    impliment firebase utils to store authenticated users into firestore

--- src/pages/SignInSignUp/SignInSignUp.js
+++ src/pages/SignInSignUp/SignInSignUp.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './SignInSignUp.scss';
 
 import SignIn from '../../components/SignIn/SignIn';
+import SignUp from '../../components/SignUp/SignUp';
+
+import './SignInSignUp.scss';
 
 const SignInSignUp = () => (
 	<div className="sign-in-and-sign-up">
 		<SignIn />
+		<SignUp />
 	</div>
 );
 
